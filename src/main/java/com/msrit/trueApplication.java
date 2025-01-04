562ae28a74e45c305388fbf3b33aca39dfec8705
Abhay <abhaykumar7007gupta@gmail.com>
2025-01-04 18:04:10 +0530

    remove unwanted folder

--- src/main/java/com/msrit/trueApplication.java
+++ src/main/java/com/msrit/trueApplication.java
@@ -1,13 +1,9 @@
 package com.msrit;
 
-import com.msrit.Entity.Course;
-import com.msrit.Entity.Student;
 import com.msrit.Entity.Todo;
 import com.msrit.Entity.User;
-import com.msrit.dao.StudentDAO;
 import com.msrit.dao.TodoDAO;
 import com.msrit.dao.UserDAO;
-import com.msrit.resources.StudentResource;
 import com.msrit.resources.TodoResources;
 import com.msrit.resources.UserResource;
 import com.msrit.resources.greet;
@@ -16,7 +12,6 @@ import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
 import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.hibernate.HibernateBundle;
-import org.hibernate.SessionFactory;
 
 public class trueApplication extends Application<trueConfiguration> {
 
@@ -32,7 +27,7 @@ public class trueApplication extends Application<trueConfiguration> {
     // Helps in setting up the connection without explicitly mentioning sessionFactory instance
 
     private final HibernateBundle<trueConfiguration> hibernate =
-            new HibernateBundle<trueConfiguration>(Student.class, Course.class, User.class, Todo.class) {
+            new HibernateBundle<trueConfiguration>(User.class, Todo.class) {
                 @Override
                 public DataSourceFactory getDataSourceFactory(trueConfiguration configuration) {
                     return configuration.getDataSourceFactory();
@@ -47,11 +42,10 @@ public class trueApplication extends Application<trueConfiguration> {
     @Override
     public void run(final trueConfiguration configuration, final Environment environment) {
         // Register resources, health checks, and more here.
-        StudentDAO studentDao = new StudentDAO(hibernate.getSessionFactory());
+
         UserDAO userDao = new UserDAO(hibernate.getSessionFactory());
         TodoDAO todoDAO = new TodoDAO(hibernate.getSessionFactory());
         environment.jersey().register(new greet());
-        environment.jersey().register(new StudentResource(studentDao));
         environment.jersey().register(new UserResource(userDao,todoDAO));
         environment.jersey().register(new TodoResources(todoDAO));
     }
